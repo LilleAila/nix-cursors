2f91166657135141e4b85383b902406290fdb644
LilleAila <olai.solsvik@gmail.com>
2024-04-22 16:34:39 +0200

    Switched to flake-parts

--- flake.nix
+++ flake.nix
@@ -1,26 +1,38 @@
 {
-  description = "Nix-cursors";
+  description = "A simple nix flake using flake-parts";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    devshell.url = "github:numtide/devshell";
+    devshell.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs }:
-  let
-    lib = nixpkgs.lib;
-    systems = [
-      "x86_64-linux"
-      "aarch64-linux"
-    ];
-    forEachSystem = f: lib.genAttrs systems (system: f pkgsFor.${system});
-    pkgsFor = lib.genAttrs systems (
-      system:
-        import nixpkgs {
-          inherit system;
-        }
-    );
-  in
-  {
-    packages = forEachSystem (pkgs: import ./pkgs {inherit pkgs;});
-  };
+  outputs = {...} @ inputs:
+    inputs.flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
+
+      imports = [
+        inputs.devshell.flakeModule
+      ];
+
+      perSystem = {
+        pkgs,
+        system,
+        ...
+      }: {
+        packages = import ./pkgs {inherit pkgs;};
+
+        devshells.default = {
+          packages = with pkgs; [
+            nil
+            alejandra
+          ];
+        };
+      };
+    };
 }
