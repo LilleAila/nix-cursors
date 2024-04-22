466b60208ac0d10cb114a6759256206a6f0c3306
LilleAila <olai.solsvik@gmail.com>
2024-04-22 18:19:22 +0200

    Moved away from flake-parts

--- flake.nix
+++ flake.nix
@@ -3,36 +3,23 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-
-    flake-parts.url = "github:hercules-ci/flake-parts";
-    devshell.url = "github:numtide/devshell";
-    devshell.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = {...} @ inputs:
-    inputs.flake-parts.lib.mkFlake {inherit inputs;} {
-      systems = [
-        "x86_64-linux"
-        "aarch64-linux"
-      ];
-
-      imports = [
-        inputs.devshell.flakeModule
-      ];
-
-      perSystem = {
-        pkgs,
-        system,
-        ...
-      }: {
-        packages = import ./pkgs {inherit pkgs;};
-
-        devshells.default = {
-          packages = with pkgs; [
-            nil
-            alejandra
-          ];
-        };
-      };
-    };
+  outputs = {nixpkgs, ...} @ inputs: let
+    lib = nixpkgs.lib;
+    systems = [
+      "x86_64-linux"
+      "aarch64-linux"
+    ];
+    forEachSystem = f: lib.genAttrs systems (system: f pkgsFor.${system});
+    pkgsFor = lib.genAttrs systems (
+      system:
+        import nixpkgs {
+          inherit system;
+        }
+    );
+  in {
+    packages = forEachSystem (pkgs: import ./pkgs {inherit pkgs;});
+    devShells = forEachSystem (pkgs: import ./shell.nix {inherit pkgs;});
+  };
 }
